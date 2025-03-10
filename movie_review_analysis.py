4387ebf10b980333d07ccaa2283d78d34d29c19e
M-Raham <rahamm711@gmail.com>
2025-03-10 23:48:44 +0500

    Processed movie reviews using TF-IDF vectorization

--- movie_review_analysis.py
+++ movie_review_analysis.py
@@ -1,6 +1,8 @@
 import nltk
 from nltk.corpus import movie_reviews
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 
 # Download dataset
 nltk.download('movie_reviews')
@@ -24,3 +26,11 @@ print(df.head())
 
 # Check class distribution
 print(df["Sentiment"].value_counts())
+
+# TF-IDF Feature Extraction
+vectorizer = TfidfVectorizer(stop_words='english', max_features=5000)
+X = vectorizer.fit_transform(df['Review'])  # Transforming text data
+y = df['Sentiment']
+
+print("✅ TF-IDF Feature Extraction Completed!")
+print(f"Feature Matrix Shape: {X.shape}")  # Should print (2000, N) where N is the number of features
